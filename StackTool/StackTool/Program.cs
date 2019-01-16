942e7e0e68c9a18bef8783713536fe5ae19d57b7
tommyhoule <tommyhoule@outlook.fr>
2019-01-16 14:34:07 -0500

    Add StackTool to Tools directory

---
+++ StackTool/StackTool/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StackTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------Stack creation--------");
+            StackTool<int> stackTool = new StackTool<int>();
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("--------printing empty stack--------");
+            stackTool.printStack();
+            Console.WriteLine("------------------------------------");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("--------printing 1 value stack (pushed 1)--------");
+            stackTool.push(3);
+            stackTool.printStack();
+            Console.WriteLine("-------------------------------------------------");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("--------printing 5 value stack (pushed 4 more)--------");
+            stackTool.push(4);
+            stackTool.push(5);
+            stackTool.push(6);
+            stackTool.push(7);
+            stackTool.printStack();
+            Console.WriteLine("------------------------------------------------------");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("--------printing 2 value stack (popped 3 more)--------");
+            stackTool.pop();
+            stackTool.pop();
+            stackTool.pop();
+            stackTool.printStack();
+            Console.WriteLine("------------------------------------------------------");
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
---
+++ StackTool/StackTool/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackTool
+{
+    class StackTool<T>
+    {
+        private ArrayList stack;
+
+        public StackTool()
+        {
+            stack = new ArrayList();
+            Console.WriteLine("Stack created !!!");
+        }
+
+        public object push(object element)
+        {
+            stack.Add(element);
+            return element;
+        }
+
+        public object pop()
+        {
+            object item = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count-1);
+            return item;
+        }
+
+        public void printStack()
+        {
+            if(stack.Count != 0)
+            {
+                ArrayList stackList = stack;
+                stackList.Reverse();
+                foreach (T item in stackList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("!!! empty stack !!!");
+            }
+        }
+    }
+}
